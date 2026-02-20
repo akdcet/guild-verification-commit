668d2f0d9e768e73c8601e070c8a2f3945ed64e7
akdcet <99772385+akdcet@users.noreply.github.com>
2026-02-20 23:05:05 +0300

    Fix database connection timeout

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T20:04:47.325Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-02-20T20:04:57.922Z
+Commit: Fix database connection timeout
