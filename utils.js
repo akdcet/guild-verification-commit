4faebff86f4db3af53fec6cbd8b0e08ccbfc24ff
akdcet <99772385+akdcet@users.noreply.github.com>
2026-02-24 20:11:28 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-24T17:11:25.060Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-02-24T17:11:28.139Z
+Commit: Resolve timezone issues in date handling
