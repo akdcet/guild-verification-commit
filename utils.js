2799a746416db49b832e47e3e85ba7de6e5ef328
akdcet <99772385+akdcet@users.noreply.github.com>
2026-02-25 21:53:00 +0300

    Add dark mode support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-25T18:52:29.821Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-02-25T18:52:59.321Z
+Commit: Add dark mode support
