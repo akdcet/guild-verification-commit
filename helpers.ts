0eeb92483a68d1cc6c682b88e058cd68ff7061f6
akdcet <99772385+akdcet@users.noreply.github.com>
2026-03-03 22:34:48 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-26T18:03:16.878Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-03T19:34:47.044Z
+Commit: Correct typo in configuration file
