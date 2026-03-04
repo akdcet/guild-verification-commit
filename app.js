f7a141e906dfc1b57ad14d67f615cda23ac90be7
akdcet <99772385+akdcet@users.noreply.github.com>
2026-03-05 00:26:46 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T19:34:45.585Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-04T21:26:45.407Z
+Commit: Fix broken links in documentation
