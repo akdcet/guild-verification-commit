4255b072213266d043ad652bf272435c007fae70
akdcet <99772385+akdcet@users.noreply.github.com>
2026-03-05 21:31:49 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:31:44.873Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T18:31:49.962Z
+Commit: Resolve timezone issues in date handling
